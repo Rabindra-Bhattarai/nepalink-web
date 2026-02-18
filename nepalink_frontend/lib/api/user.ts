377041b957232ddac764bd26d6151b2cd02ee3eb
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-02-18 11:04:36 +0545

    user

---
+++ nepalink_frontend/lib/api/user.ts
@@ -0,0 +1,25 @@
+import axiosInstance from "./axios";
+import { API } from "./endpoints";
+import { getAuthToken } from "../cookie";
+
+export const updateProfile = async (userId: string, data: any) => {
+  const token = await getAuthToken();
+  const res = await axiosInstance.put(`${API.USERS}/${userId}`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
+
+export const updateProfilePic = async (userId: string, file: File) => {
+  const token = await getAuthToken();
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const res = await axiosInstance.put(`${API.USERS}/${userId}/profile-pic`, formData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return res.data;
+};
